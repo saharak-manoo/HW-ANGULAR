1963edc96581374bcd8a6ed7c25d11839e642738
Kraiwut Arayangkura <kraiwuthi.a@gmail.com>
2018-01-05 13:56:30 +0700

    add home screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated :user do
+    root to: 'home#index'
+  end
+
+  root to: redirect('/users/sign_in')
 end
