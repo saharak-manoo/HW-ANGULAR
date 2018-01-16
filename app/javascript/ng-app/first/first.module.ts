d3bfa3319768415db9ed7e75a28b0d508e3622ce
Kraiwut Arayangkura <kraiwuthi.a@gmail.com>
2018-01-16 13:57:26 +0700

    remove unused code

--- app/javascript/ng-app/first/first.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { FirstPage } from './first.html';
-
-@NgModule({
-  declarations: [
-    FirstPage
-  ]
-})
-export class FirstModule {}
--- app/javascript/ng-app/second/second.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SecondPage } from './second.html';
-
-@NgModule({
-  declarations: [
-    SecondPage
-  ]
-})
-export class SecondModule {}
